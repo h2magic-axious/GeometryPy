4e3453a5920e66664bbcdcab7e97f3eb76bcdb23
soleperson <h2magic@126.com>
2020-10-28 17:55:30 +0800

    remove polar line

--- mathpy/geometry2d/line2d.py
+++ mathpy/geometry2d/line2d.py
@@ -1,4 +1,10 @@
-class LineCartesian2d:
+from math import hypot
+
+from mathpy.vector import Vector
+from mathpy.geometry2d.point2d import PointCartesian2d
+
+
+class Line2D:
     def __init__(self, a, b, c):
         self.A = a
         self.B = b
@@ -7,6 +13,18 @@ class LineCartesian2d:
     def __repr__(self):
         return f"Line: {self.A} x + {self.B} y + {self.C} = 0"
 
-class LinePolar:
-    def __init__(self):
-        pass
+    def vector(self):
+        return Vector(-self.B, self.A, 0)
+
+    def distance_with_point(self, point: PointCartesian2d):
+        abs_value = abs(self.A * point.x + self.B * point.y + self.C)
+        hyp_value = hypot(self.A, self.B)
+
+        return abs_value / hyp_value
+
+    def vertical_with(self, line):
+        if isinstance(line, Line2D):
+            v = line.vector()
+            return v * self.vector() == 0
+        else:
+            return False
